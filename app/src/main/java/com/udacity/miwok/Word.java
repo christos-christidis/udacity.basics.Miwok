bdf46c0a34ae8f95230de3823fa10205572f5a88
Christos Christidis <christos.christidis86@gmail.com>
2019-08-20 08:24:17 +0300

    images and visual polish

--- app/src/main/java/com/udacity/miwok/Word.java
+++ app/src/main/java/com/udacity/miwok/Word.java
@@ -2,12 +2,22 @@ package com.udacity.miwok;
 
 class Word {
 
+    private static final int NO_IMAGE_PROVIDED = -1;
+
     private final String mDefaultTranslation;
     private final String mMiwokTranslation;
+    private final int mImageResourceId;
 
     Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mImageResourceId = NO_IMAGE_PROVIDED;
+    }
+
+    Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     String getDefaultTranslation() {
@@ -16,5 +26,13 @@ class Word {
 
     String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/udacity/miwok/WordAdapter.java
+++ app/src/main/java/com/udacity/miwok/WordAdapter.java
@@ -3,18 +3,23 @@ package com.udacity.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 class WordAdapter extends ArrayAdapter<Word> {
 
-    WordAdapter(Context context, List<Word> words) {
+    private final int mColorResourceId;
+
+    WordAdapter(Context context, List<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -27,12 +32,24 @@ class WordAdapter extends ArrayAdapter<Word> {
 
         Word word = getItem(position);
         if (word != null) {
+            ImageView imageView = listItemView.findViewById(R.id.image);
+            if (word.hasImage()) {
+                imageView.setImageResource(word.getImageResourceId());
+                imageView.setVisibility(View.VISIBLE);
+            } else {
+                imageView.setVisibility(View.GONE);
+            }
+
             TextView miwokTextView = listItemView.findViewById(R.id.miwok_text_view);
             miwokTextView.setText(word.getMiwokTranslation());
 
             TextView defaultTextView = listItemView.findViewById(R.id.default_text_view);
             defaultTextView.setText(word.getDefaultTranslation());
 
+            View textContainer = listItemView.findViewById(R.id.text_container);
+            // SOS: note how we must first get the color the color-resId maps to!
+            int color = ContextCompat.getColor(getContext(), mColorResourceId);
+            textContainer.setBackgroundColor(color);
         }
 
         return listItemView;
